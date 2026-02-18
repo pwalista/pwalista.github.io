7bc3615431c0e3d178c2124b086dc43e0a493db5
Luis Eduardo <luiseduardoddm@gmail.com>
2026-02-17 23:29:45 -0600

    Actualización4

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
  * conoce como número menor y se cambia cuando se realizan
  * modificaciones menores.
  */
-const VERSION = "1.03"
+const VERSION = "1.04"
 
 /**
  * Nombre de la carpeta de caché.
@@ -76,7 +76,6 @@ const ARCHIVOS = [
  "js/lib/resaltaSiEstasEn.js",
  "js/lib/custom/md-app-bar.js",
  "ungap/custom-elements.js",
- "/"
 ]
 
 // Verifica si el código corre dentro de un service worker.
