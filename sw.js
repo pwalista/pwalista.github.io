9071b993f94ff3a474cdeafdc47f392dcdde560e
Luis Eduardo <luiseduardoddm@gmail.com>
2026-02-17 22:39:48 -0600

    Actualización

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
  * conoce como número menor y se cambia cuando se realizan
  * modificaciones menores.
  */
-const VERSION = "1.01"
+const VERSION = "1.02"
 
 /**
  * Nombre de la carpeta de caché.
