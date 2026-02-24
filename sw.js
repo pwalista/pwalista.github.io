a9c2afdc35b2fb0bc793ca07b1861a93a24b0faa
Luis Eduardo <luiseduardoddm@gmail.com>
2026-02-24 17:31:55 -0600

    Actualización css md-list

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
  * conoce como número menor y se cambia cuando se realizan
  * modificaciones menores.
  */
-const VERSION = "1.06"
+const VERSION = "1.10"
 
 /**
  * Nombre de la carpeta de caché.
